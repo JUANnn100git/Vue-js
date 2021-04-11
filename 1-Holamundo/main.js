8d446be5f8566c9c3e93190ee5085ba0f2539d64
Juan Villanueva <jvavillanuevaaguinaga@gmail.com>
2021-04-11 13:11:02 -0500

    11-04-21

--- 1-Holamundo/main.js
+++ 1-Holamundo/main.js
@@ -2,9 +2,35 @@ const app = Vue.createApp({
     data() {
         return {
             titulo: 'Mi Banco con Vue.js',
-            cantidad: 0,
+            cantidad: 500,
             enlace: 'https://youtube.com/bluuweb',
-            estado: false
+            estado: false,
+            servicios: ['transferencias', 'pagos', 'giros', 'cheques'],
+            desactivar: false
+        }
+    },
+    methods: {
+        agregarSaldo() {
+            if (this.cantidad === 0) {
+                this.desactivar = false
+            }
+            this.cantidad = this.cantidad + 100
+        },
+        disminuirSaldo(valor) {
+            if (this.cantidad === 0) {
+                this.desactivar = true
+                alert("El saldo ya llegó a 0")
+                return
+            }
+            this.cantidad = this.cantidad - valor
+        }
+    },
+    computed: {
+        colorCantidad() {
+            return this.cantidad > 500 ? 'text-success' : 'text-danger'
+        },
+        mayusculastexto() {
+            return this.titulo.toUpperCase()
         }
     }
 })
